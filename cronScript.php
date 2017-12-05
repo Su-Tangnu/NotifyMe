46c7385721a39ebf856b1f02006cdab3367058e9
MatthewManning <matthewd.manning@gmail.com>
2017-12-05 07:08:09 -0600

    cronScript.php will check websites for updates and send out the appropriate emails when run using cron.

---
+++ cronScript.php
@@ -0,0 +1,49 @@
+<?php
+  $conn = mysqli_connect("localhost","root","","notifyme_db");
+  $sql_urls_info = "SELECT * FROM urls";
+  $execute_urls_info = mysqli_query($conn,$sql_urls_info);
+  if($execute_urls_info){
+    while($data = mysqli_fetch_assoc($execute_urls_info)){
+      $headers = get_headers($data['url'], 1);
+      if($headers["Last-Modified"] && ($headers["Last-Modified"] == $data["last-modified"])){
+        $sql_update = "UPDATE urls SET updated = 0 WHERE url = $data[url]";
+        $execute_update = mysqli_query($conn,$sql_update);
+        echo "Not updated based on Last-Modified.";
+      }
+      elseif ($headers["ETag"] && ($headers["ETag"] == $data["etag"])) {
+        $sql_update = "UPDATE urls SET updated = 0 WHERE url = $data[url]";
+        $execute_update = mysqli_query($conn,$sql_update);
+        echo "Not updated based on Etag.";
+      }
+      elseif ($headers["Last-Modified"]){
+        $lastModified = "Last-Modified";
+        $sql_update = "UPDATE urls SET last-modified = $headers[$lastModified], updated = 1 WHERE url = $data[url]";
+        $execute_update = mysqli_query($conn,$sql_update);
+        echo "Updated $data[url] based on Last-Modified.";
+      }
+      else{
+        $sql_update = "UPDATE urls SET etag = $headers[ETag], updated = 1 WHERE url = $data[url]";
+        $execute_update = mysqli_query($conn,$sql_update);
+        echo "Updated $data[url] based on ETag.";
+      }
+    }
+  }
+
+  $sql_users_to_email = "SELECT users.email
+                         FROM users
+                         INNER JOIN user_url_list ON users.email = user_url_list.email
+                         INNER JOIN urls ON urls.url = user_url_list.url
+                         WHERE urls.updated = 1";
+  $execute_users_to_email = mysqli_query($conn,$sql_users_to_email);
+  if($execute_users_to_email){
+    while($emails = mysqli_fetch_assoc($execute_urls_info)){
+      // the message
+      $msg = "You have websites that have updated!\n
+              Come see which ones at NotifyMe!";
+      // use wordwrap() if lines are longer than 70 characters
+      $msg = wordwrap($msg,70);
+      // send email
+      $sent = mail($emails['email'],"Websites Have Updated!",$msg);
+    }
+  }
+?>
