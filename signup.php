33917a09d85b3256f7b93086fe65c8d9e33e0db8
MatthewManning <matthewd.manning@gmail.com>
2017-12-06 13:40:54 -0600

    Changed the way that email addresses and passwords were handled as well as how the user logs out.  Now, cannot access userHomepage without signing in.

--- signup.php
+++ signup.php
@@ -1,3 +1,6 @@
+<?php
+  session_start();
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -16,12 +19,15 @@
         	//$db = "notifyme_db";
         	//mysqli_connect($server,$username,$password,$db);
         	$conn = mysqli_connect("localhost","root","","notifyme_db");
+        	//$conn  mysql_select_db("notifyme_db");
         	if( isset($_POST['email']) && isset($_POST['pass']) ){
         		$Email = mysqli_real_escape_string($conn,strip_tags($_POST['email']));
+            $_SESSION["email"] = $Email;
         		$Password = mysqli_real_escape_string($conn,strip_tags($_POST['pass']));
+            $_SESSION["pass"] = $Password;
         		$run_sqlCreateAccount = "INSERT INTO users (email, password) VALUES ('$Email', '$Password')";
         		if(mysqli_query($conn,$run_sqlCreateAccount)){
-              echo "<script>window.location = \"userHomepage.php/?email=$Email\"; </script>";
+              echo "<script>window.location = \"userHomepage.php/\"; </script>";
         		}
         		else {
               ?>
