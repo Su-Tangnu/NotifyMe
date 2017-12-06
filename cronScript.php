0265b9e521f312346ed3816fcc7e9232713844ce
MatthewManning <matthewd.manning@gmail.com>
2017-12-05 20:57:10 -0600

    Fixed conditionals in cronScript in case Last-Modified or ETag is not in HTTP Header.

--- cronScript.php
+++ cronScript.php
@@ -5,26 +5,34 @@
   if($execute_urls_info){
     while($data = mysqli_fetch_assoc($execute_urls_info)){
       $headers = get_headers($data['url'], 1);
-      if($headers["Last-Modified"] && ($headers["Last-Modified"] == $data["last-modified"])){
-        $sql_update = "UPDATE urls SET updated = 0 WHERE url = $data[url]";
+      if(array_key_exists("Last-Modified", $headers) && ($headers["Last-Modified"] == $data["last-modified"])){
+        $sql_update = "UPDATE urls SET updated = '0' WHERE url = $data[url]";
         $execute_update = mysqli_query($conn,$sql_update);
-        echo "Not updated based on Last-Modified.";
+        if($execute_update){
+          echo "Not updated based on Last-Modified.";
+        }
       }
-      elseif ($headers["ETag"] && ($headers["ETag"] == $data["etag"])) {
-        $sql_update = "UPDATE urls SET updated = 0 WHERE url = $data[url]";
+      elseif (array_key_exists("ETag", $headers) && ($headers["ETag"] == $data["etag"])) {
+        $sql_update = "UPDATE urls SET updated = '0' WHERE url = $data[url]";
         $execute_update = mysqli_query($conn,$sql_update);
-        echo "Not updated based on Etag.";
+        if($execute_update){
+          echo "Not updated based on Etag.";
+        }
       }
-      elseif ($headers["Last-Modified"]){
+      elseif (array_key_exists("Last-Modified", $headers)){
         $lastModified = "Last-Modified";
-        $sql_update = "UPDATE urls SET last-modified = $headers[$lastModified], updated = 1 WHERE url = $data[url]";
+        $sql_update = "UPDATE urls SET last-modified = '$headers[$lastModified]', updated = '1' WHERE url = $data[url]";
         $execute_update = mysqli_query($conn,$sql_update);
-        echo "Updated $data[url] based on Last-Modified.";
+        if($execute_update){
+          echo "Updated $data[url] based on Last-Modified.";
+        }
       }
-      else{
-        $sql_update = "UPDATE urls SET etag = $headers[ETag], updated = 1 WHERE url = $data[url]";
+      elseif(array_key_exists("ETag", $headers)){
+        $sql_update = "UPDATE urls SET etag = '$headers[ETag]', updated = '1' WHERE url = $data[url]";
         $execute_update = mysqli_query($conn,$sql_update);
-        echo "Updated $data[url] based on ETag.";
+        if($execute_update){
+          echo "Updated $data[url] based on ETag.";
+        }
       }
     }
   }
@@ -34,16 +42,15 @@
                          INNER JOIN user_url_list ON users.email = user_url_list.email
                          INNER JOIN urls ON urls.url = user_url_list.url
                          WHERE urls.updated = 1";*/
-  $sql_users_to_email = "SELECT user_url_list.email 
+  $sql_users_to_email = "SELECT user_url_list.email
                          FROM user_url_list
                          INNER JOIN urls ON urls.url = user_url_list.url
-                         WHERE urls.updated = 1";					 
+                         WHERE urls.updated = 1";
   $execute_users_to_email = mysqli_query($conn,$sql_users_to_email);
   if($execute_users_to_email){
     while($emails = mysqli_fetch_assoc($execute_users_to_email)){
       // the message
-      $msg = "You have websites that have updated!\n
-              Come see which ones at NotifyMe!";
+      $msg = "You have websites that have updated!\nCome see which ones at NotifyMe!";
       // use wordwrap() if lines are longer than 70 characters
       $msg = wordwrap($msg,70);
       // send email
