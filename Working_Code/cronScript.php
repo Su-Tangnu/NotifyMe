0c2a32773a8cbab0deb54b851d7ee2272fc7b995
Su-Tangnu <tangnu.suveni@gmail.com>
2017-12-05 21:43:55 -0600

    Working_Code

---
+++ Working_Code/cronScript.php
@@ -0,0 +1,61 @@
+<?php
+  $conn = mysqli_connect("localhost","root","","notifyme_db");
+  $sql_urls_info = "SELECT * FROM urls";
+  $execute_urls_info = mysqli_query($conn,$sql_urls_info);
+  if($execute_urls_info){
+    while($data = mysqli_fetch_assoc($execute_urls_info)){
+      $headers = get_headers($data['url'], 1);
+      if($headers["Last-Modified"] && ($headers["Last-Modified"] == $data["last-modified"])){
+        $sql_update = 'UPDATE urls SET updated = 0 WHERE url = "$data[url]"';
+        $execute_update = mysqli_query($conn,$sql_update);
+		if($execute_update){
+        echo "Not updated based on Last-Modified.";}
+      }
+      elseif ($headers["ETag"] && ($headers["ETag"] == $data["etag"])) {
+        $sql_update = 'UPDATE urls SET updated = 0 WHERE url = "$data[url]"';
+        $execute_update = mysqli_query($conn,$sql_update);
+			if($execute_update){
+			echo "Not updated based on Etag.";}
+      }
+      elseif ($headers["Last-Modified"]){
+        $lastModified = "Last-Modified";
+        $sql_update = 'UPDATE urls SET last-modified = $headers[$lastModified], updated = 1 WHERE url = "$data[url]"';
+        $execute_update = mysqli_query($conn,$sql_update);
+			if($execute_update){
+			echo "Updated $data[url] based on Last-Modified.";}
+      }
+      else{
+        $sql_update = 'UPDATE urls SET etag = $headers[ETag], updated = 1 WHERE url = "$data[url]"';
+        $execute_update = mysqli_query($conn,$sql_update);
+			if($execute_update){
+			echo "Updated $data[url] based on ETag.";}
+      }
+    }
+  }
+
+ /* $sql_users_to_email = "SELECT users.email
+                         FROM users
+                         INNER JOIN user_url_list ON users.email = user_url_list.email
+                         INNER JOIN urls ON urls.url = user_url_list.url
+                         WHERE urls.updated = 1";*/
+  /*$sql_users_to_email = "SELECT *
+                         FROM user_url_list
+                         INNER JOIN urls ON urls.url = user_url_list.url
+                         WHERE urls.updated = 1";	*/
+  $sql_users_to_email = "SELECT *
+                         FROM user_url_list , urls
+                         WHERE  urls.url = user_url_list.url
+                         AND urls.updated = 1";							 
+						 
+  $execute_users_to_email = mysqli_query($conn,$sql_users_to_email);
+  if($execute_users_to_email){
+    while($emails_url = mysqli_fetch_assoc($execute_users_to_email)){
+      // the message
+      $msg = $emails_url['url'];
+      // use wordwrap() if lines are longer than 70 characters
+      $msg = wordwrap($msg,70);
+      // send email
+      $sent = mail($emails_url['email'],"Websites Have Updated!",$msg);
+    }
+  }
+?>
---
+++ Working_Code/signup.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>NotifyMe! Sign Up</title>
+    <link rel="stylesheet" type="text/css" href="default.css">
+  </head>
+  <body>
+    <div class="homepage">
+      <div class="form">
+        <div class="title">
+          Welcome!
+        </div>
+        <br>
+        <?php
+        	//$server = "localhost";
+        	//$db = "notifyme_db";
+        	//mysqli_connect($server,$username,$password,$db);
+        	$conn = mysqli_connect("localhost","root","","notifyme_db");
+        	//$conn  mysql_select_db("notifyme_db");
+        	if( isset($_POST['email']) && isset($_POST['pass']) ){
+        		$Email = mysqli_real_escape_string($conn,strip_tags($_POST['email']));
+        		$Password = mysqli_real_escape_string($conn,strip_tags($_POST['pass']));
+        		$run_sqlCreateAccount = "INSERT INTO users (email, password) VALUES ('$Email', '$Password')";
+        		if(mysqli_query($conn,$run_sqlCreateAccount)){
+              echo "<script>window.location = \"userHomepage.php/?email=$Email\"; </script>";
+        		}
+        		else {
+              ?>
+              <div class="failure">
+                Sign up failed!  That email address already has an account.
+                Please enter a new email address and try again!
+              </div>
+              <?php
+        		}
+          }
+        ?>
+        <form method="post" class="login-form">
+          <input type="text" name="email" placeholder="Email"/>
+          <input type="password" name="pass" placeholder="Password"/>
+          <input type="submit" placeholder="SIGN UP"/>
+        </form>
+        <p class="message">Already a user? <a href="index.php">Log In</a></p>
+      </div>
+    </div>
+  </body>
+</html>
