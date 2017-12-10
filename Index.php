6b4c6865b7597feeddc5c0d195ae6b9dda55a484
MatthewManning <matthewd.manning@gmail.com>
2017-12-10 17:09:10 -0600

    Added comments and cleaned up code.

--- Index.php
+++ Index.php
@@ -1,6 +1,12 @@
 <?php
+  //We use sessions to ensure that people can only see the homepage
+  //once they have logged in.
   session_start();
+
+  //mysqli_connect(server,username,password,database)
+  $conn = mysqli_connect("localhost","root","","notifyme_db");
 ?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -15,47 +21,81 @@
         </div>
         <br>
         <?php
-        	//$server = "localhost";
-        	//$db = "notifyme_db";
-        	//mysqli_connect($server,$username,$password,$db);
-        	$conn = mysqli_connect("localhost","root","","notifyme_db");
-        	//$conn  mysql_select_db("notifyme_db");
-        	if( isset($_POST['email']) && isset($_POST['pass']) ){
-        		$Email = mysqli_real_escape_string($conn,strip_tags($_POST['email']));
+          //If the user provided an email or username (that is not blank)
+          //and password, try signing them up.
+          if(isset($_POST['email']) && isset($_POST['pass']) && $_POST['email'] != ''){
+            //This variable actually can be the email or username.
+            //strip_tags and mysqli_real_escape_string to help prevent sql injection.
+            $Email = mysqli_real_escape_string($conn,strip_tags($_POST['email']));
+            //Session variable used to store email and verify that the user has logged in:
             $_SESSION["email"] = $Email;
+
             //$Pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
             $Pass = $_POST['pass'];
         		$Password = mysqli_real_escape_string($conn,strip_tags($Pass));
+            //Session variable used to store password and verify that the user has logged in:
             $_SESSION["pass"] = $Password;
+
+            //Get all users that match the email or username entered in the "email" variable.
         		$run_sqlEmailId = "SELECT * FROM users WHERE email = '$Email' OR username = '$Email'";
-        		if(mysqli_query($conn,$run_sqlEmailId)){  //Check if for an emailId there is a row
-          		$run_sqlCorrectLogin = "SELECT * FROM users  WHERE (email = '$Email' AND password='$Password') OR (username = '$Email' AND password='$Password')";
+            //Run the query and check that their is an email or username with the email value.
+            if(mysqli_query($conn,$run_sqlEmailId)){
+              //Get all users that match the email or username entered in the "email" variable
+              //and match the password entered.
+              $run_sqlCorrectLogin = "SELECT * FROM users  WHERE (email = '$Email' AND password='$Password') OR (username = '$Email' AND password='$Password')";
           		$result_sqlCorrectLogin = mysqli_query($conn,$run_sqlCorrectLogin);
               $count = mysqli_num_rows($result_sqlCorrectLogin);
-          		if($count >= 1) {
+              //if there was (at least) one instance where user ID (email or username)
+              //and password matched, log the in and send them to their homepage.
+              if($count >= 1) {
           			echo "<script>window.location = \"userHomepage.php\"; </script>";
           		}
           		else {
-                $_POST["login"] = "false";
+                //login failed, so set login to false.
+                $_POST['login'] = "false";
+                //Clears the $_SESSION array since the credentials were invalid.
+                session_unset();
                 ?>
                 <div class="message">
                   Login failed!
                 </div>
                 <div class="submessage">
+                  That email or username is not in our database.
                   Please check your credentials and try again!
                 </div>
                 <?php
           		}
         		}
             else{
-              $_POST["login"] = "false";
+              //login failed, so set login to false.
+              $_POST['login'] = "false";
+              //Clears the $_SESSION array since the credentials were invalid.
+              session_unset();
               ?>
               <div class="message">
-                Login failed!  That email or username is not in our database.
+                Login failed!
+              </div>
+              <div class="submessage">
+                That email or username is not in our database.
                 Please check your credentials and try again!
               </div>
               <?php
             }
+          }
+          elseif(isset($_POST['email']) && $_POST['email'] == ''){
+            //login failed, so set login to false.
+            $_POST['login'] = "false";
+            //Clears the $_SESSION array since the credentials were invalid.
+            session_unset();
+            ?>
+            <div class="message">
+              Login failed!
+            </div>
+            <div class="submessage">
+              You must enter an email or username.
+              Please check your credentials and try again!
+            </div>
+            <?php
           }
         ?>
         <form method="post">
@@ -65,7 +105,9 @@
         </form>
         <div class="submessage">
         <?php
-          if(isset($_POST["login"])){
+          //If "login" is set in post, it is set to failed since that is the only
+          //possible assigned value.  Since login failed, offer them account recovery.
+          if(isset($_POST['login'])){
             ?>
             Having trouble remembering your password?
             </br>
--- signup.php
+++ signup.php
@@ -2,6 +2,9 @@
   //We use sessions to ensure that people can only see the homepage
   //once they have logged in.
   session_start();
+
+  //mysqli_connect(server,username,password,database)
+  $conn = mysqli_connect("localhost","root","","notifyme_db");
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,29 +20,39 @@
         </div>
         <br>
         <?php
-        	//$server = "localhost";
-        	//$db = "notifyme_db";
-        	//mysqli_connect($server,$username,$password,$db);
-        	$conn = mysqli_connect("localhost","root","","notifyme_db");
-        	//$conn  mysql_select_db("notifyme_db");
-        	if( isset($_POST['email']) && isset($_POST['pass']) ){
-        		$Email = mysqli_real_escape_string($conn,strip_tags($_POST['email']));
+          //If the user provided an email (that is not blank)
+          //and password, try signing them up.
+        	if(isset($_POST['email']) && isset($_POST['pass']) && $_POST['email'] != ''){
+            //strip_tags and mysqli_real_escape_string to help prevent sql injection.
+            $Email = mysqli_real_escape_string($conn,strip_tags($_POST['email']));
+            //Session variable used to store email and verify that the user has logged in:
             $_SESSION["email"] = $Email;
+
             //$Pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
             $Pass = $_POST['pass'];
         		$Password = mysqli_real_escape_string($conn,strip_tags($Pass));
             $run_sqlCreateAccount = "INSERT INTO users (email, username, password) VALUES ('$Email', '$Email', '$Password')";
+            //Session variable used to store password and verify that the user has logged in:
             $_SESSION["pass"] = $Password;
+            //If the user provided a username (that is not blank), set it
+            //and add it to the INSERT statement where appropriate.
             if(isset($_POST['username']) && $_POST['username'] != ''){
               $Username = mysqli_real_escape_string($conn,strip_tags($_POST["username"]));
               $_SESSION["username"] = $Username;
               $run_sqlCreateAccount = "INSERT INTO users (email, username, password) VALUES ('$Email', '$Username', '$Password')";
             }
+            //Run the insertion.  If it succeeds, take the user to their homepage.
         		if(mysqli_query($conn,$run_sqlCreateAccount)){
               echo "<script>window.location = \"userHomepage.php\"; </script>";
         		}
+            //Else, if it fails, but the email and password can be inserted,
+            //it must be the username that is the problem.
         		elseif(mysqli_query($conn,"INSERT INTO users (email, password) VALUES ('$Email', '$Password')")){
+              //We must delete the email and password we just inserted
+              //since the user hasn't been signed up.
               mysqli_query($conn,"DELETE FROM users WHERE (email='$Email' AND password='$Password')");
+              //Clears the $_SESSION array since sign up failed.
+              session_unset();
               ?>
               <div class="message">
                 Sign up failed!  That username is already taken.
@@ -47,7 +60,11 @@
               </div>
               <?php
         		}
+            //If we failed to sign up the user and the username wasn't the problem,
+            //the email address must already be taken.
             else{
+              //Clears the $_SESSION array since sign up failed.
+              session_unset();
               ?>
               <div class="message">
                 Sign up failed!
@@ -59,6 +76,20 @@
               </div>
               <?php
             }
+          }
+          //If the user tried to sign up without providing an email,
+          //reject them and tell them that they must provide an email address.
+          elseif(isset($_POST['email']) && $_POST['email'] == ''){
+            ?>
+            <div class="message">
+              Sign up failed!
+            </div>
+            <div class="submessage">
+              You must enter an email address!
+              </br>
+              Please choose an email address and try again!
+            </div>
+            <?php
           }
         ?>
         <form method="post" class="login-form">
