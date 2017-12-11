eda86641d11c112662d4b81419a4e0f70c0d823d
MatthewManning <matthewd.manning@gmail.com>
2017-12-10 20:46:05 -0600

    Cleaned code and added commenting.

--- passwordChange.php
+++ passwordChange.php
@@ -1,5 +1,10 @@
 <?php
+  //We use sessions to ensure that people can only see the homepage
+  //once they have logged in.
   session_start();
+
+  //mysqli_connect(server,username,password,database)
+  $conn = mysqli_connect("localhost","root","","notifyme_db");
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,23 +20,31 @@
         </div>
       </br>
         <?php
-        	//$server = "localhost";
-        	//$db = "notifyme_db";
-        	//mysqli_connect($server,$username,$password,$db);
-        	$conn = mysqli_connect("localhost","root","","notifyme_db");
-        	//$conn  mysql_select_db("notifyme_db");
+          //If the old password is provided, alongside the new password
+          //and its verification.
         	if( isset($_POST['oldPass']) && isset($_POST['newPass']) && isset($_POST['newPassVerify'])){
-        		if($_POST['newPass'] == $_POST['newPassVerify']){  //Check that the new passwords are the same
+            //Check that the new password and the verification are the same.
+            if($_POST['newPass'] == $_POST['newPassVerify']){
               //$OldPass = password_hash($_POST['oldPass'], PASSWORD_DEFAULT);
               $OldPass = $_POST['oldPass'];
-              $run_sqlCheckOldPass = "SELECT * FROM users WHERE email = '$_SESSION[email]' AND password='$OldPass'";
-          		$result_sqlCheckOldPass = mysqli_query($conn,$run_sqlCheckOldPass);
+              //Check that the old password is accurate.
+              //SQL to get rows with old password and email.
+              $sqlCheckOldPass = "SELECT * FROM users WHERE email = '$_SESSION[email]' AND password='$OldPass'";
+              //Run the SQL statement.
+              $result_sqlCheckOldPass = mysqli_query($conn,$sqlCheckOldPass);
+
+              //Get the count of how many rows were in the result.
               $count = mysqli_num_rows($result_sqlCheckOldPass);
+              //If count is 1, the password is right.
           		if($count == 1) {
                 //$NewPass = password_hash($_POST['newPass'], PASSWORD_DEFAULT);
                 $NewPass = $_POST['newPass'];
-                $run_sqlChangePass = "UPDATE users SET password = '$NewPass' where email = '$_SESSION[email]'";
-            		if($result_sqlChangePass = mysqli_query($conn,$run_sqlChangePass)){
+                //SQL to set the new password.
+                $sqlChangePass = "UPDATE users SET password = '$NewPass' where email = '$_SESSION[email]'";
+                //Run the SQL statement.
+                $result_sqlChangePass = mysqli_query($conn,$sqlChangePass);
+                //If we were able to change the password, tell the user.
+            		if($result_sqlChangePass){
                   ?>
                   <div class="message">
                     Password change successful!
@@ -43,6 +56,7 @@
                   </div>
                   <?php
                 }
+                //Otherwise, tell the user that we couldn't change the password.
                 else{
                   ?>
                   <div class="message">
@@ -55,6 +69,8 @@
                   <?php
                 }
           		}
+              //Otherwise, the old password provided was wrong.
+              //Tell the user and allow them to go back.
           		else {
                 ?>
                 <div class="message">
@@ -70,6 +86,7 @@
                 <?php
           		}
         		}
+            //Otherwise, the new password and verification don't match.
             else{
               ?>
               <div class="message">
