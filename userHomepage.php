f025c81391ac4700bd87a60e6d2b29d31791e0dc
MatthewManning <matthewd.manning@gmail.com>
2017-12-10 19:15:19 -0600

    Cleaned code and added commenting.

--- userHomepage.php
+++ userHomepage.php
@@ -2,20 +2,29 @@
 	//We use sessions to ensure that people can only see the homepage
 	//once they have logged in.
 	session_start();
-	//$server = "localhost";
-	//$db = "notifyme_db";
-	//mysqli_connect($server,$username,$password,$db);
+
+	//mysqli_connect(server,username,password,database)
 	$conn = mysqli_connect("localhost","root","","notifyme_db");
-	//$conn  mysql_select_db("notifyme_db");
+
+	//If the user is signed in (email and password are set),
+	//set the user's info.
 	if(array_key_exists("email", $_SESSION) && array_key_exists("pass", $_SESSION)){
+		//Technically, $_SESSION["email"] can have either the email or username of the user,
+		//so we call it UserID.
 		$UserId = $_SESSION["email"];
 		$Password = $_SESSION["pass"];
-		$run_sqlGetUserInfo = "SELECT * FROM users  WHERE (email = '$UserId' AND password='$Password') OR (username = '$UserId' AND password='$Password')";
-		$result_sqlGetUserInfo = mysqli_query($conn,$run_sqlGetUserInfo);
-		$result_UserInfo = mysqli_fetch_assoc($result_sqlGetUserInfo);
-		$Username = $result_UserInfo["username"];
-		$Email = $result_UserInfo["email"];
+		//SQL command to get the user's info.
+		$sqlGetUserInfo = "SELECT * FROM users  WHERE (email = '$UserId' AND password='$Password') OR (username = '$UserId' AND password='$Password')";
+		//Run the previous SQL command.
+		$result_sqlGetUserInfo = mysqli_query($conn,$sqlGetUserInfo);
+		//Get the user's info in an associative array.
+		$UserInfo = mysqli_fetch_assoc($result_sqlGetUserInfo);
+		//Get the username and email from UserInfo.
+		$Username = $UserInfo["username"];
+		$Email = $UserInfo["email"];
 	}
+	//Otherwise, the user isn't logged in.
+	//Send them back to the log in page (index.php).
 	else{
 		echo "<script>window.location = \"/NotifyMe/Index.php\"; </script>";
 	}
@@ -38,19 +47,21 @@
 			</br>
 			</div>
 				<?php
+					//If the user is editing a URL, give them the edit form.
 					if(isset($_GET['edit_id'])){
 				?>
 						<div class="body">
 							Edit URL
 						</div>
-						<form class="col-md-6" method="post">
-								<input type="text" name= "editURL" value="<?php echo $_GET['edit_id']; ?>" class="form-control" required/>
-								<input type="hidden" name="edit_URL_hide" value="<?php echo $_GET['edit_id']?>"/>
+						<form method="post">
+								<input type="text" name= "editURL" value="<?php echo $_GET['edit_id']; ?>" required/>
 								<input type="submit" name="edit_URL" value="Change"/>
 								<input type="submit" name="edit_CANCEL" value="Cancel"/>
 						</form>
 				<?php
 					}
+					//Otherwise, display the normal user's homepage, giving access to
+					//logging the user out, changing their password, and adding other URLs.
 					else{
 				?>
 						<div class="body">
@@ -87,13 +98,11 @@
 					<h3>List of Websites</h3>
 				</div>
 				<?php
-					$sql = "SELECT * FROM user_url_list WHERE email = '$Email'";
-					$execute = mysqli_query($conn,$sql);
 					echo"
 						<table class='table'>
 							<thead>
 								<tr>
-									<th>S.No</th>
+									<th>URL Number</th>
 									<th>Existing URL</th>
 									<th>Visit</th>
 									<th>Edit</th>
@@ -102,27 +111,34 @@
 							</thead>
 						<tbody>
 						";
+						//URL number
 						$count=1;
+						//Get the user's list of websites and put them in the table.
+						$sql = "SELECT * FROM user_url_list WHERE email = '$Email'";
+						$execute = mysqli_query($conn,$sql);
+						//If the user's list returns anything, put it in the table.
 						if($execute){
+							//Loop through the data per row with an associative array.
 							while($data = mysqli_fetch_assoc($execute)){
-								/*echo $data['URL'];*/
 								echo "
 								<tr>
 									<td>$count</td>
 									<td>$data[url]</td>";
+								//Add http:// to the url if it's not there so that the link works.
 								if((substr($data['url'],0,4)!='HTTP')&&(substr($data['url'],0,4)!='http')&&(substr($data['url'],0,4)!='HTTPS')&&(substr($data['url'],0,4)!='https')){
 									echo "<td><a href='http://";
 									echo $data['url'];
 									echo"'>Visit this page</td>";
 								}
+								//Otherwise, it already had http, so you don't need to add it.
 								else{
 									echo "<td><a href='";
 									echo $data['url'];
 									echo"'>Visit this page</td>";
 								}
 								echo"
-									<td><a href='/NotifyMe/userHomepage.php?edit_id=$data[url]' class='btn btn-success'>Edit</button></td>
-									<td><a href='/NotifyMe/userHomepage.php?del_id=$data[url]' class='btn btn-danger'>Delete</button></td>
+									<td><a href='/NotifyMe/userHomepage.php?edit_id=$data[url]'>Edit</td>
+									<td><a href='/NotifyMe/userHomepage.php?del_id=$data[url]'>Delete</td>
 								</tr>
 								";
 								$count++;
@@ -135,67 +151,109 @@
 	</body>
 </html>
 <?php
-
+	//If we have a link to add, add it to the database.
 	if( isset($_POST['submit_URL']) ){
-			$newURLVal = mysqli_real_escape_string($conn,strip_tags($_POST['newURL']));
-			$sql_urls = "INSERT INTO urls (url) VALUES ('$newURLVal')";
-			$sql_user_url_list = "INSERT INTO user_url_list (email, url) VALUES ('$Email','$newURLVal')";
-			$run_sql_insert_url = mysqli_query($conn , $sql_urls);
-			$run_sql_insert_user_url_list = mysqli_query($conn , $sql_user_url_list);
-			$properURL = $newURLVal;
-      if((substr($properURL,0,4)!='HTTP')&&(substr($properURL,0,4)!='http')&&(substr($properURL,0,4)!='HTTPS')&&(substr($properURL,0,4)!='https')){
-        $properURL = "http://" . $properURL;
+		//sanitize the new URL string.
+		$newURLVal = mysqli_real_escape_string($conn,strip_tags($_POST['newURL']));
+		//SQL to insert the new URL into the database both in the url table and
+		//in the user_url_list table.
+		$sql_urls = "INSERT INTO urls (url) VALUES ('$newURLVal')";
+		$sql_user_url_list = "INSERT INTO user_url_list (email, url) VALUES ('$Email','$newURLVal')";
+		//Run the previous two queries with the next two statements.
+		$run_sql_insert_url = mysqli_query($conn , $sql_urls);
+		$run_sql_insert_user_url_list = mysqli_query($conn , $sql_user_url_list);
+
+		//properURL is the new URL, but it is guaranteed to have http://
+		//in front of it with the following conditional.
+		$properURL = $newURLVal;
+    if((substr($properURL,0,4)!='HTTP')&&(substr($properURL,0,4)!='http')&&(substr($properURL,0,4)!='HTTPS')&&(substr($properURL,0,4)!='https')){
+      $properURL = "http://" . $properURL;
+    }
+		//get the headers from the new URL as an associative array.
+		//(The 1 as the second argument means return it as an associative array.)
+		$headers = get_headers($properURL, 1);
+		//If the headers were returned, check that they have either Last-Modified
+		//or an ETag.  If not, delete it from the database and tell the user that
+		//we cannot add it because it does not have the necessary headers.
+    if($headers){
+			//If Last-Modified exists, set it for the URL in the database.
+			if (array_key_exists("Last-Modified", $headers)){
+        $lastModified = "Last-Modified";
+        $sql_update = "UPDATE urls SET lastModified = '$headers[$lastModified]' WHERE url = '$newURLVal'";
+        $execute_update = mysqli_query($conn,$sql_update);
+        /*if($execute_update){
+          echo "Updated $newURLVal Last-Modified.";
+          echo "</br>";
+        }*/
       }
-			$headers = get_headers($properURL, 1);
-      if($headers){
-				if (array_key_exists("Last-Modified", $headers)){
-          $lastModified = "Last-Modified";
-          $sql_update = "UPDATE urls SET lastModified = '$headers[$lastModified]' WHERE url = '$newURLVal'";
-          $execute_update = mysqli_query($conn,$sql_update);
-          if($execute_update){
-            echo "Updated $newURLVal Last-Modified.";
-            echo "</br>";
-          }
-        }
-        if(array_key_exists("ETag", $headers)){
-          $sql_update = "UPDATE urls SET etag = '$headers[ETag]' WHERE url = '$newURLVal'";
-          $execute_update = mysqli_query($conn,$sql_update);
-          if($execute_update){
-            echo "Updated $newURLVal ETag.";
-            echo "</br>";
-          }
-        }
-				elseif(!array_key_exists("Last-Modified", $headers)){
-					$run_delete_url = mysqli_query($conn,"DELETE FROM urls WHERE url = '$newURLVal'");
-				}
+			//If ETag exists, set it for the URL in the database.
+      if(array_key_exists("ETag", $headers)){
+        $sql_update = "UPDATE urls SET etag = '$headers[ETag]' WHERE url = '$newURLVal'";
+        $execute_update = mysqli_query($conn,$sql_update);
+        /*if($execute_update){
+          echo "Updated $newURLVal ETag.";
+          echo "</br>";
+        }*/
+      }
+			//If both Last-Modified and ETag don't exist, delete the new URL
+			//from the database (and tell the user that we can't add it).
+			elseif(!array_key_exists("Last-Modified", $headers)){
+				$run_delete_url = mysqli_query($conn,"DELETE FROM urls WHERE url = '$newURLVal'");
 			}
-			if($run_sql_insert_user_url_list && !$run_delete_url){
-				echo "<script>window.location = \"/NotifyMe/userHomepage.php\"; </script>";
-			}
-			else{
-				?>
-				<script>alert("URL was not added!\n\nSorry, this URL does not have a Last-Modified value or an Etag value.")</script>
-				<?php
-			}
+		}
+		//If we cannot get the headers, delete the URL from the database and tell the user that
+		//we cannot add it because it does not have the necessary headers.
+		else{
+			$run_delete_url = mysqli_query($conn,"DELETE FROM urls WHERE url = '$newURLVal'");
+		}
+		//If we added the new URL to the database and didn't later delete it,
+		//go back to/refresh the homepage so that it shows up in the table.
+		if($run_sql_insert_user_url_list && !$run_delete_url){
+			echo "<script>window.location = \"/NotifyMe/userHomepage.php\"; </script>";
+		}
+		//Otherwise, tell the user that we were unable to add the URL.
+		else{
+			?>
+			<script>alert("URL was not added!\n\nSorry, this URL does not have a Last-Modified value or an Etag value.")</script>
+			<?php
+		}
 	}
 
+	//If the user decides to cancel editing the URL, go back to the homepage.
 	if(isset($_POST['edit_CANCEL'])){
 				echo "<script>window.location = \"/NotifyMe/userHomepage.php\"; </script>";
 	}
-	if(isset($_POST['edit_URL'])){
+	//If the user wants to edit a URL, (edit_URL indicates the user wants to change it)
+	//change the old value, edit_id to the new value, editURL.
+	if(isset($_POST['edit_URL']) && isset($_POST['editURL'])){
+		//Sanitize the edited URL.
 		$editdURL = mysqli_real_escape_string($conn,strip_tags($_POST['editURL']));
+		//SQL to update the URL in both the urls and user_url_list tables.
 		$edit_sql_urls = "UPDATE urls SET url = '$editdURL' WHERE url='$_GET[edit_id]' ";
-		$run_sql_insert_url = mysqli_query($conn , $edit_sql_urls);
 		$edit_sql_user_url_list = "UPDATE user_url_list SET url = '$editdURL' WHERE email='$Email' AND url='$_GET[edit_id]'";
+		//Run the previous two queries.
+		$run_sql_insert_url = mysqli_query($conn , $edit_sql_urls);
 		$run_sql_insert_user_url_list = mysqli_query($conn , $edit_sql_user_url_list);
-		if($run_sql_insert_url){
+		//If the URL was updated in the tables, go to/refresh the homepage.
+		if($run_sql_insert_user_url_list){
 				echo "<script>window.location = \"/NotifyMe/userHomepage.php\"; </script>";
 		}
 	}
+	//If the user wants to delete the URL, delete it from the database AND
+	//refresh the homepage.
 	if(isset($_GET['del_id'])){
+		//SQL to delete the del_id url from the urls and user_url_list tables.
 		$del_sql = "DELETE FROM urls WHERE url = '$_GET[del_id]'";
 		$del_sql_user_url_list = "DELETE FROM user_url_list WHERE url= '$_GET[del_id]'";
-		if(mysqli_query($conn , $del_sql) && mysqli_query($conn , $del_sql_user_url_list)){
+
+		//Run the previous two sql statements.
+		$run_del_sql = mysqli_query($conn , $del_sql);
+		$run_del_sql_user_url_list = mysqli_query($conn , $del_sql_user_url_list);
+		
+		//If we are successful in deleting from the urls, refresh the homepage.
+		//Even if we are unsuccessful in deleting from user_url_list,
+		//cascade will delete it for us.
+		if($run_del_sql){
 				echo "<script>window.location = \"/NotifyMe/userHomepage.php\"; </script>";
 		}
 	}
