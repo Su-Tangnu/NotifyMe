5b1b4ce6c2b4d35e82c0e15387ebdf714d53b537
MatthewManning <matthewd.manning@gmail.com>
2017-12-10 20:46:44 -0600

    Cleaned code and added commenting.

--- logout.php
+++ logout.php
@@ -1,8 +1,12 @@
 <?php
+  //We have to start the session in order to
+  //remove the session variables and destroy the session.
   session_start();
-  // remove all session variables
+
+  //Clears the $_SESSION array, signing the user out.
   session_unset();
-  // destroy the session
+
+  //Not necessary, but proper form as it deletes all of the session's data.
   session_destroy();
 ?>
 <!DOCTYPE html>
@@ -12,13 +16,21 @@
     <link rel="stylesheet" type="text/css" href="default.css">
   </head>
   <body>
-    <div class=form>
-      You have been successfully logged out.
-      </br>
-      Click
-      <a href="/NotifyMe/Index.php">
-        here
-      </a>
-      to log back in.
+    <div class="homepage">
+      <div class="form">
+        <div class="title">
+          Logged Out!
+        </div>
+        <div class="submessage">
+          </br>
+          You have been successfully logged out.
+          </br>
+          Click
+          <a href="/NotifyMe/Index.php">
+            here
+          </a>
+          to log back in.
+        </div>
+      </div>
     </div>
   </body>
