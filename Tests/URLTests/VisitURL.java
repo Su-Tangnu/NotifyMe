1f7c0abab0e2bc1f109fa918618aec9948d129a9
MatthewManning <matthewd.manning@gmail.com>
2017-12-11 21:42:50 -0600

    Tests for adding, editing, deleting and visiting URLs.

---
+++ Tests/URLTests/VisitURL.java
@@ -0,0 +1,78 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class VisitURL {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testVisitURL() throws Exception {
+    driver.get("http://localhost/NotifyMe/");
+    driver.findElement(By.id("emailInput")).click();
+    driver.findElement(By.id("emailInput")).clear();
+    driver.findElement(By.id("emailInput")).sendKeys("mdmanning@gmail.com");
+    driver.findElement(By.id("passInput")).click();
+    driver.findElement(By.id("passInput")).clear();
+    driver.findElement(By.id("passInput")).sendKeys("asdf");
+    driver.findElement(By.id("loginInput")).click();
+    driver.findElement(By.id("linkToURL1")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
